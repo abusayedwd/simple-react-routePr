57dff04578c72eb8509a7cb093a0d25cffbefa0c
abusayedwd <110084857+abusayedwd@users.noreply.github.com>
2023-04-03 16:45:06 +0600

    aded navlink

--- src/components/FriendDetails/FriendDetail.jsx
+++ src/components/FriendDetails/FriendDetail.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, useNavigate } from 'react-router-dom';
 
 const FriendDetail = () => {
         const detail = useLoaderData()
         // console.log(detail)
+        const navigate = useNavigate()
+        const detailBack = () => {
+                navigate(-1)
+        }
         return (
                 <div>
                         <h3>Everything all details show</h3>
                         <h1> name : {detail.name} </h1>
                         <p> phone : {detail.phone}</p>
-                        <small>email: {detail.email}</small>
+                        <small>email: {detail.email}</small> <br />
+                        <button onClick={detailBack}>Back</button>
 
                 </div>
         );
